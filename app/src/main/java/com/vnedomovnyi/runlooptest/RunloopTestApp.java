06297a42018a373a4e2fc77ddcbe1f2d9a5d1d5a
Vladislav Nedomovnyi <nedomovnyvlad@gmail.com>
2020-08-13 20:14:48 +0300

    Enable logging

--- app/src/main/java/com/vnedomovnyi/runlooptest/RunloopTestApp.java
+++ app/src/main/java/com/vnedomovnyi/runlooptest/RunloopTestApp.java
@@ -2,6 +2,17 @@ package com.vnedomovnyi.runlooptest;
 
 import android.app.Application;
 
+import timber.log.Timber;
+
 public class RunloopTestApp extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        if (BuildConfig.DEBUG) {
+            Timber.plant(new Timber.DebugTree());
+        }
+    }
 
 }
