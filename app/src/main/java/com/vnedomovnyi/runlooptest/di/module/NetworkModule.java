7e30cb4b1ffd6fe8353485c5b1c87401fec11f78
Vladislav Nedomovnyi <nedomovnyvlad@gmail.com>
2020-08-15 11:24:23 +0300

    Implement HTTP logging

--- app/src/main/java/com/vnedomovnyi/runlooptest/di/module/NetworkModule.java
+++ app/src/main/java/com/vnedomovnyi/runlooptest/di/module/NetworkModule.java
@@ -5,15 +5,25 @@ import javax.inject.Singleton;
 import dagger.Module;
 import dagger.Provides;
 import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
+import timber.log.Timber;
 
 @Module
 public class NetworkModule {
 
     @Provides
     @Singleton
-    OkHttpClient provideOkHttpClient() {
+    OkHttpClient provideOkHttpClient(HttpLoggingInterceptor httpLoggingInterceptor) {
         return new OkHttpClient.Builder()
+                .addInterceptor(httpLoggingInterceptor)
                 .build();
+    }
+
+    @Provides
+    @Singleton
+    HttpLoggingInterceptor provideHttpLoggingInterceptor() {
+        return new HttpLoggingInterceptor(s -> Timber.tag("OkHttp").d(s))
+                .setLevel(HttpLoggingInterceptor.Level.BASIC);
     }
 
 }
